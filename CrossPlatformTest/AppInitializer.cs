46c5ded58721df599bd1da62fc49a11345f872c1
AswathKumar123 <aswathkumar@zoho.com>
2020-09-28 16:09:03 -0500

    Initial check-in of module EAXamarin

---
+++ CrossPlatformTest/AppInitializer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace CrossPlatformTest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            // TODO: If the iOS or Android app being tested is included in the solution 
+            // then open the Unit Tests window, right click Test Apps, select Add App Project
+            // and select the app projects that should be tested.
+            //
+            // The iOS project should have the Xamarin.TestCloud.Agent NuGet package
+            // installed. To start the Test Cloud Agent the following code should be
+            // added to the FinishedLaunching method of the AppDelegate:
+            //
+#if ENABLE_TEST_CLOUD
+              Xamarin.Calabash.Start();
+#endif
+            if (platform == Platform.iOS)
+            {
+                return ConfigureApp
+                    .iOS
+                    // TODO: Update this path to point to your Android app and uncomment the
+                    // code if the app is not included in the solution.
+                    //.ApkFile (@"/Users/aswathkumarkulasekarapandian/Desktop/Appium Learn Mobile Automation Testing_v5332.0_apkpure.com.apk")
+                    .DeviceIdentifier("32D0FA54-CD0E-4C1D-861D-F3916560C5A7")
+                    .InstalledApp("com.companyname.eaxamarin")
+                    .StartApp();
+            }
+else
+            return ConfigureApp
+                    .Android
+                    // TODO: Update this path to point to your iOS app and uncomment the
+                    // code if the app is not included in the solution.
+                    //.AppBundle ("../../../iOS/bin/iPhoneSimulator/Debug/XamarinForms.iOS.app")
+                    .InstalledApp("com.companyname.eaxamarin")
+                    .StartApp();
+            }
+        }
+    }
+    
---
+++ CrossPlatformTest/Tests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace CrossPlatformTest
+{
+    //[TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+
+        //[Test]
+        //public void NewTest()
+        //{
+        //   // app.Repl();
+
+        //    app.Tap(x => x.Text("First item"));
+        //    app.Back();
+        //    app.Tap(x => x.Text("Second item"));
+    //}
+
+        [Test]
+        public void TestMethod()
+        {
+            
+
+            app.Tap(x => x.Id("content"));
+            app.Back();
+            app.Tap(x => x.Id("action_bar_root"));
+            app.Back();
+            app.Tap(x => x.Text("Add"));           
+            app.EnterText(x => x.Text("Item name"), "Seventh Item");
+            app.DismissKeyboard();
+            app.Tap(x => x.Marked("Save"));
+            app.Tap(x => x.Text("Add"));           
+            app.EnterText(x => x.Text("Item name"), "Eight Item");
+            app.DismissKeyboard();
+            app.Tap(x => x.Marked("Save"));
+            app.Tap(x => x.Text("Add"));           
+            app.EnterText(x => x.Text("Item name"), "9th Item");
+            app.DismissKeyboard();
+            app.Tap(x => x.Marked("Save"));
+            app.Tap(x => x.Text("Add"));                       
+            app.EnterText(x => x.Text("Item name"), "10th Item");
+            app.DismissKeyboard();
+            app.Tap(x => x.Marked("Save"));
+            app.Tap(x => x.Text("Add"));                     
+            app.EnterText(x => x.Text("Item name"), "11th Item");
+            app.DismissKeyboard();
+            app.Tap(x => x.Marked("Save"));
+            app.Tap(x => x.Text("Add"));                    
+            app.EnterText(x => x.Text("Item name"), "12th Item");
+            app.DismissKeyboard();
+            app.Tap(x => x.Marked("Save"));
+            app.Tap(x => x.Text("Add"));                    
+            app.EnterText(x => x.Text("Item name"), "13th Item");
+            app.DismissKeyboard();
+            app.Tap(x => x.Marked("Save"));
+            app.Tap(x => x.Text("Add"));                    
+            app.EnterText(x => x.Text("Item name"), "14th Item");
+            app.DismissKeyboard();
+            app.Tap(x => x.Marked("Save"));
+            app.Tap(x => x.Text("Add"));                       
+            app.EnterText(x => x.Text("Item name"), "15th Item");
+            app.DismissKeyboard();
+            app.Tap(x => x.Marked("Save"));
+            app.Tap(x => x.Text("Add"));          
+            app.EnterText(x => x.Text("Item name"), "16th Item");
+            app.DismissKeyboard();
+            app.Tap(x => x.Marked("Save"));
+            app.WaitForElement(x => x.Text("Item name10th Item"));
+            app.ScrollDownTo(x => x.Text("Item name15th Item"));
+            var elementCount = app.Query(x => x.Id("action_bar_root").All().Text("Item name13th Item")).Count();
+            //app.Repl();
+            Assert.That(elementCount, Is.EqualTo(1), "This element is not present");
+            app.ScrollUpTo(x => x.Text("First item"));
+            app.SwipeLeftToRight();
+            app.SwipeRightToLeft();
+            var elementCount2 = app.Query(x => x.Id("action_bar_root").All().Text("Third item")).Count();
+            Assert.That(elementCount2, Is.EqualTo(1), "This element is not present");
+
+
+
+
+
+
+
+
+        }
+
+
+        
+    }
+}
